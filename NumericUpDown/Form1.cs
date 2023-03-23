f9903ad61064bd95af5bcc7884650416bd49dbc9
Nguyen Tan Tien <112912834+Ngoi-sao-co-don@users.noreply.github.com>
2023-03-23 13:05:44 +0700

    Add files via upload

---
+++ NumericUpDown/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NumericUpDown
+{
+    public partial class frmNumericUpDown : Form
+    {
+        public frmNumericUpDown()
+        {
+            InitializeComponent();
+        }
+
+        private void frmNumericUpDown_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
